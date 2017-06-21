dbe5bcdec1c591ac4a0db43329df229e3d397c9f
Dave <daves125125@gmail.com>
2017-06-21 23:14:39 +0100

    initial

--- zipkin-server/src/main/java/com/djh/tracing/ApplicationLoader.java
+++ zipkin-server/src/main/java/com/djh/tracing/ApplicationLoader.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.instrument.web.client.TraceWebClientAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 /**
  * @author David Hancock
  */
-@EnableScheduling
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ApplicationLoader {
 
@@ -23,33 +24,6 @@ public class ApplicationLoader {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationLoader.class);
-    }
-
-    /**
-     * We explicitly declare this as a Bean to allow Spring to enrich RestTemplate
-     * with a traceRestTemplateInterceptor - {@link TraceWebClientAutoConfiguration}
-     */
-    @Bean
-    public RestOperations restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Component
-    public class MessageProducer {
-
-        private RestOperations restOperations;
-
-        public MessageProducer(RestOperations restOperations) {
-            this.restOperations = restOperations;
-        }
-
-        @Scheduled(initialDelay = 3000, fixedRate = 3000)
-        public void produceMessage() {
-
-            String payload = "Payload Contents: " + Math.random();
-            LOG.info("[Publisher] Sending message with payload: [{}]", payload);
-            restOperations.put("http://middleman:8080/foo", payload);
-        }
     }
 
 }
